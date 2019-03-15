747adea9f6c8d260cd9bab06cec527d1dabe0fd8
Eren Kızılay <32452198+ErenKizilay@users.noreply.github.com>
2019-03-15 09:50:55 +0300

    Update zenossActionExecutor.py

--- Zenoss/zenossActionExecutor.py
+++ Zenoss/zenossActionExecutor.py
@@ -10,19 +10,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-payload', '--queuePayload', help='Payload from queue', required=True)
 parser.add_argument('-apiKey', '--apiKey', help='The apiKey of the integration', required=True)
 parser.add_argument('-opsgenieUrl', '--opsgenieUrl', help='The url', required=True)
-parser.add_argument('-loglevel', '--loglevel', help='Log level', required=True)
+parser.add_argument('-logLevel', '--logLevel', help='Log Level', required=True)
 parser.add_argument('-command_url', '--command_url', help='The url', required=False)
 parser.add_argument('-username', '--username', help='Username', required=False)
 parser.add_argument('-password', '--password', help='Password', required=False)
 args = vars(parser.parse_args())
 
-logging.basicConfig(stream=sys.stdout, level=args['loglevel'])
+logging.basicConfig(stream=sys.stdout, level=args['logLevel'])
 
 
 def parse_field(key, mandatory):
-    variable = queue_message[key]
-    if not variable.strip():
-        variable = args[key]
+    variable = queue_message.get(key)
+    if not variable:
+        variable = args.get(key)
     if mandatory and not variable:
         logging.error(LOG_PREFIX + " Skipping action, Mandatory conf item '" + key +
                       "' is missing. Check your configuration file.")
@@ -32,7 +32,7 @@ def parse_field(key, mandatory):
 
 
 def parse_timeout():
-    parsed_timeout = args['http.timeout']
+    parsed_timeout = args.get('http.timeout')
     if not parsed_timeout:
         return 30000
     return int(parsed_timeout)
@@ -53,26 +53,26 @@ def main():
 
     LOG_PREFIX = "[" + action + "]"
 
-    logging.info("Will execute " + action + " for alertId " + alert_id)
+    logging.info("Will execute " + str(action) + " for alertId " + str(alert_id))
 
     username = parse_field('username', True)
     password = parse_field('password', True)
     url = parse_field('command_url', True)
 
-    logging.debug("Username: " + username)
-    logging.debug("Command Url: " + url)
-    logging.debug("AlertId: " + alert_id)
-    logging.debug("Source: " + source)
-    logging.debug("Action: " + action)
+    logging.debug("Username: " + str(username))
+    logging.debug("Command Url: " + str(url))
+    logging.debug("AlertId: " + str(alert_id))
+    logging.debug("Source: " + str(source))
+    logging.debug("Action: " + str(action))
 
     token = base64.b64encode((username + ":" + password).encode('US-ASCII'))
     headers = {
         "Content-Type": "application/json",
         "Accept-Language": "application/json",
-        "Authorization": "Basic " + str(token)
+        "Authorization": "Basic " + bytes(token).decode("US-ASCII")
     }
     if alert_id:
-        opsgenie_alert_api_url = args['opsgenieUrl'] + "/" + alert_id
+        opsgenie_alert_api_url = args['opsgenieUrl'] + "/v2/alerts/" + alert_id
         opsgenie_alert_api_headers = {
             "Content-Type": "application/json",
             "Accept-Language": "application/json",
@@ -83,7 +83,7 @@ def main():
             post_params = {
                 "action": "EventsRouter",
                 "data": {
-                    "evids": [alert_response.json()["alias"]]
+                    "evids": [alert_response.json()["data"]["alias"]]
                 },
                 "type": "rpc",
                 "tid": alert_id
@@ -110,9 +110,9 @@ def main():
                     logging.debug("Zenoss response: " + str(response.content))
                 else:
                     logging.warning(
-                        "Could not execute at Zenoss. Zenoss Response: " + response.content + " Status Code: " + response.status_code)
+                        "Could not execute at Zenoss. Zenoss Response: " + str(response.content) + " Status Code: " + str(response.status_code))
         else:
-            logging.warning("Alert with id [" + alert_id + "] does not exist in OpsGenie. It is probably deleted.")
+            logging.warning("Alert with id [" + str(alert_id) + "] does not exist in OpsGenie. It is probably deleted.")
     else:
         logging.warning("Alert id does not exist ")
 
